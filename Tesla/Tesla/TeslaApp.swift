1e92dbc9f0206e85a228d4ef69e3de9f373bbd28
@Helen853 <dudarevalen_1997@mail.ru>
2024-05-13 18:34:02 +0500

    Создан начальный проект

---
+++ Tesla/Tesla/TeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaApp.swift
+//  Tesla
+//
+//  Created by Киса Мурлыса on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
